3a5751e30d9fb1d54d20607874a0eff9faf52b50
ALISSON JOSE <jose.5989@aluno.pr.senac.br>
2023-09-11 11:10:22 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/teste_repositorio/Teste_repositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste_repositorio;
+
+/**
+ *
+ * @author jose.5989
+ */
+public class Teste_repositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
